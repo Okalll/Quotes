bb59bfd9a17b6ebc09a767ca4c9a0af9e6078939
Okall Vivian <vivieokall@gmail.com>
2019-01-14 10:29:25 +0300

    "Modify files"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesFormComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quotes-form/quotes-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/core';
+
+@Component({
+  selector: 'app-quotes-form',
+  templateUrl: './quotes-form.component.html',
+  styleUrls: ['./quotes-form.component.css']
+})
+export class QuotesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
